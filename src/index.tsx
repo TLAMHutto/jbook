67c1ace364d0d375672c43512688574a3115f93d
TLAMHutto <keatonhutto8@gmail.com>
2022-07-06 20:24:42 -0500

    stuff

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDom from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-pllugin';
+
+
 
 const App = () => {
     const ref = useRef<any>();
@@ -21,14 +24,14 @@ const App = () => {
         if (!ref.current) {
             return;
         }
-         const result = await ref.current.transform(
-            input, {
-                loader: 'jsx',
-                target: 'es2015',
-
-            }
-        );
-        setCode(result.code);
+         const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()]
+         })
+        
+        setCode(result.outputFiles[0].text);
     }
 
     return <div>
