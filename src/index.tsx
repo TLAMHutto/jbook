84a669804965c4450e2dfa96fbbd5c97abfbc002
TLAMHutto <keatonhutto8@gmail.com>
2022-07-07 21:36:58 -0500

    stuff

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,52 @@
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
-import ReactDom from 'react-dom';
-import { unpkgPathPlugin } from './plugins/unpkg-path-pllugin';
-
-
+import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
-    const ref = useRef<any>();
-    const [input, setInput] = useState('');
-    const [code, setCode] = useState('');
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: '/esbuild.wasm',
-        });
-        
-    }
-    useEffect(() => {startService()}, [])
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+  useEffect(() => {
+    startService();
+  }, []);
 
-
-    const onClick = async () => {
-        if (!ref.current) {
-            return;
-        }
-         const result = await ref.current.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin()]
-         })
-        
-        setCode(result.outputFiles[0].text);
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
     }
 
-    return <div>
-        <textarea value={input}onChange={e => setInput(e.target.value)} ></textarea>
-        <div>
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
+
+    // console.log(result);
+
+    setCode(result.outputFiles[0].text);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
         <button onClick={onClick}>Submit</button>
-        </div>
-        <pre>
-            {code}
-        </pre>
-    </div>;
-    }
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
 
-    ReactDom.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
